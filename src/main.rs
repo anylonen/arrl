864367de44c5512715928f4bd6268d12941600c5
Anssi Ylönen <anylonen@gmail.com>
2025-09-14 00:58:25 +0300

    Updated bevy and bevy_inspector-egui. Added bevy_egui

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(EguiPlugin { enable_multipass_for_primary_context: true })
-    .add_plugins(WorldInspectorPlugin::new())
-    .add_systems(Update, hello_world_system)
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin::default())
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Update, hello_world_system)
+        .run();
 }
 
 fn hello_world_system() {
